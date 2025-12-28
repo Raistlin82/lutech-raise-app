c25b8a47528a230a5a14d3847c5f85fd3c3324a4
Raistlin82 <gabriele.rendina@gmail.com>
2025-12-28 20:11:04 +0100

    feat: integrate CustomerProvider in app root
    - Wrap app with CustomerProvider
    - Ensure CustomerStore available throughout app

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,17 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { SettingsProvider } from './stores/SettingsStore'
+import { CustomerProvider } from './stores/CustomerStore'
 import { OpportunitiesProvider } from './stores/OpportunitiesStore'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <SettingsProvider>
-      <OpportunitiesProvider>
-        <App />
-      </OpportunitiesProvider>
+      <CustomerProvider>
+        <OpportunitiesProvider>
+          <App />
+        </OpportunitiesProvider>
+      </CustomerProvider>
     </SettingsProvider>
   </StrictMode>,
 )
